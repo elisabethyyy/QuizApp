5b18304d47b94378c98ec097e01aebfeebc344fb
elisabethyyy <60440710+elisabethyyy@users.noreply.github.com>
2022-03-28 08:50:32 +0300

    111
    111

--- server.py
+++ server.py
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
     
+@app.route('/about')
+def about():
+  return render_template ("about.html")
+
+
 #Pārbaudes lapa, lai saprastu, ka kods vispār strādā
 @app.route('/health')
 def health():
